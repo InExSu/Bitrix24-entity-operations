2ef0ef962b62d6af4a0aa45883f87291b6105550
InExSu <InExSu@bk.ru>
2022-07-01 10:36:34 +0300

    handLocal

---
+++ repo_new.cmd
@@ -0,0 +1,7 @@
+echo "# Bitrix24-entity-operations" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/InExSu/Bitrix24-entity-operations.git
+git push -u origin main
